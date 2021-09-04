e0088941cfd66fbd5cfa2b7c20ffd1bcd0c00293
Lucas Fernandes da Costa <lucas@lucasfcosta.com>
2021-09-04 17:00:07 +0100

    respond pings with guildId

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
-fn main() {
-    println!("Hello, world!");
+use serenity::async_trait;
+use serenity::client::{Client, Context, EventHandler};
+use serenity::model::channel::Message;
+use serenity::framework::standard::{
+    StandardFramework,
+    CommandResult,
+    macros::{
+        command,
+        group
+    }
+};
+
+use std::env;
+
+#[group]
+#[commands(ping)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {}
+
+#[tokio::main]
+async fn main() {
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("-"))
+        .group(&GENERAL_GROUP);
+
+    let token = env::var("DISCORD_TOKEN").expect("token");
+
+
+    let mut client = Client::builder(token)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    if let Err(why) = client.start_shards(2).await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    let guild_id = match msg.guild_id {
+        Some(guild_id) => guild_id.0,
+        None => {
+            println!("Can't find guild_id");
+            0
+        },
+    };
+
+    if guild_id == 0 { return Ok(()) };
+
+    msg.reply(ctx, format!("{}", guild_id)).await?;
+    Ok(())
 }
